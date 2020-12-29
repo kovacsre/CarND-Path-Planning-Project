4d1f23712e447dd4aece650ebd38ca796cf48d43
kovacsre <reka.horvath.kovacs@gmail.com>
2020-12-29 02:41:00 -0800

    initial commit

--- src/helpers.h
+++ src/helpers.h
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <string>
 #include <vector>
+#include "spline.h"
 
 // for convenience
 using std::string;
@@ -154,4 +155,194 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s,
   return {x,y};
 }
 
+/*Calculate cost of a lane change from speed point of view
+ * inputs:  target speed - the speed we want to reach
+ * 			check_speed - the speed of the lane we want to change to
+ * 			current_speed - our current speed
+ */
+double inefficiency_cost(double target_speed, double check_speed, double current_speed) {
+	double cost = 1 - (check_speed / target_speed);
+	return cost;
+}
+
+/*Generate trajectory using spline.h
+ *
+ */
+void generate_trajectory(double car_x, double car_y, double car_yaw, double car_s, double ref_vel,
+						 int lane, int prev_size, vector<double> &previous_path_x, vector<double> &previous_path_y,
+						 vector<double> &map_waypoints_s, vector<double> &map_waypoints_x, vector<double> &map_waypoints_y,
+						 vector<double> &next_x_vals, vector<double> &next_y_vals) {
+
+
+	//define widely spaced x,y points along a bigger distance -now 30m
+	vector<double> ptsx;
+	vector<double> ptsy;
+	//reference x,y,yaw sates to see where the car is
+	//either use the reference points or the last point of the previous path
+	double ref_x = car_x;
+	double ref_y = car_y;
+	double ref_yaw = deg2rad(car_yaw);
+
+	//if previous path size is almost empty use the reference state
+	if(prev_size < 2){
+	  //use two points that make the path tangent to the car
+	  double prev_car_x = car_x - cos(car_yaw);
+	  double prev_car_y = car_y - sin(car_yaw);
+
+	  ptsx.push_back(prev_car_x);
+	  ptsx.push_back(car_x);
+
+	  ptsy.push_back(prev_car_y);
+	  ptsy.push_back(car_y);
+	}
+	else{
+	  //reference state is the end point of the previous path
+	  ref_x = previous_path_x[prev_size-1];
+	  ref_y = previous_path_y[prev_size-1];
+
+	  //use two points that make the path tangent to the previous path end points
+	  double ref_x_prev = previous_path_x[prev_size-2];
+	  double ref_y_prev = previous_path_y[prev_size-2];
+	  ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+
+	  ptsx.push_back(ref_x_prev);
+	  ptsx.push_back(ref_x);
+
+	  ptsy.push_back(ref_y_prev);
+	  ptsy.push_back(ref_y);
+	}
+	//add 3 points ahead of the vehicle 30 ms one after the other in Frenet coordinates
+	vector<double> next_wp0 = getXY(car_s+30,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+	vector<double> next_wp1 = getXY(car_s+60,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+	vector<double> next_wp2 = getXY(car_s+90,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+
+	ptsx.push_back(next_wp0[0]);
+	ptsx.push_back(next_wp1[0]);
+	ptsx.push_back(next_wp2[0]);
+
+	ptsy.push_back(next_wp0[1]);
+	ptsy.push_back(next_wp1[1]);
+	ptsy.push_back(next_wp2[1]);
+
+	//transform to the car's local coordinates = last point of previous path is (x,y) = (0,0) yaw is 0, too
+	for (int i=0; i < ptsx.size(); i++){
+	  //shift car reference angle to 0 degrees
+	  double shift_x = ptsx[i]-ref_x;
+	  double shift_y = ptsy[i]-ref_y;
+
+	  ptsx[i] = (shift_x*cos(0-ref_yaw)-shift_y*sin(0-ref_yaw));
+	  ptsy[i] = (shift_x*sin(0-ref_yaw)+shift_y*cos(0-ref_yaw));
+	}
+
+	//create a spline
+	tk::spline s;
+
+	//set x,y points to the spline
+	s.set_points(ptsx,ptsy);
+
+	//start with the previous path points from last time
+	for(int i=0; i<previous_path_x.size(); i++){
+	  next_x_vals.push_back(previous_path_x[i]);
+	  next_y_vals.push_back(previous_path_y[i]);
+
+	}
+
+	//calculate how to break up the spline so that the car travels with the intended reference velocity
+	double target_x = 30.0;
+	double target_y = s(target_x);
+	double target_dist = sqrt((target_x)*(target_x)+(target_y)*(target_y));
+
+	double x_add_on = 0;
+
+	//fill up the rest of the points of the path planner after adding the remaining points of the previous path
+	for(int i =1; i<=50-previous_path_x.size(); i++){
+
+	  double N = (target_dist/(.02*ref_vel/2.24)); //number of path points to be generated on the spline
+	  double x_point = x_add_on+(target_x)/N;
+	  double y_point = s(x_point);
+
+	  x_add_on = x_point;
+
+	  double x_ref = x_point;
+	  double y_ref = y_point;
+
+	  //returning to "normal" coordinates: rotation back
+	  x_point = (x_ref*cos(ref_yaw)-y_ref*sin(ref_yaw));
+	  y_point = (x_ref*sin(ref_yaw)+y_ref*cos(ref_yaw));
+	  //shift back
+	  x_point += ref_x;
+	  y_point += ref_y;
+
+	  next_x_vals.push_back(x_point);
+	  next_y_vals.push_back(y_point);
+	}
+
+}
+
+/*Choose next state depending on current state and state machine
+ *
+ */
+vector<string> successor_states(string state, int lane) {
+	vector<string> next_states;
+	next_states.push_back("KL");
+	if(state.compare("KL")==0){
+		if(lane > 0){
+			next_states.push_back("PLCL");
+		}
+		if(lane < 2){
+			next_states.push_back("PLCR");
+		}
+	}else if(state.compare("PLCL")==0){
+		if(lane > 0){
+			//next_states.push_back("PLCL");
+			next_states.push_back("LCL");
+		}
+	}else if(state.compare("PLCR")==0){
+		if (lane < 2){
+			//next_states.push_back("PLCR");
+			next_states.push_back("LCR");
+		}
+	}
+	return next_states;
+}
+
+/*Check if the lane we want to change to is clear
+ * and calculate its speed
+ */
+bool is_lane_clear(vector<vector<double>> &sensor_fusion, int lane_to_check, double car_s, int prev_size, double &lane_speed){
+	bool way_is_clear = true;
+	double sum_speed=0;
+	int num_of_cars = 0;
+
+	for(int i = 0; i < sensor_fusion.size(); i++){
+	  float d = sensor_fusion[i][6]; //checking the i-th car's d position
+
+	  //for cars in the lane lane to check
+	  if(d < (2+4*lane_to_check+2) && d > (2+4*lane_to_check-2)){
+		  double vx = sensor_fusion[i][3];
+		  double vy = sensor_fusion[i][4];
+		  double check_speed = sqrt(vx*vx+vy*vy)*2.24; //car speed along the road (magnitude of the speed vector) + convert to mph
+		  double check_car_s = sensor_fusion[i][5];
+
+		  check_car_s += ((double)prev_size*.02*check_speed/2.24); //trying to predict where the car will be based on velocity
+
+		  //if it will be close (from a little bit behind to a little bit ahead) then the lane is not clear
+		  if((check_car_s >= car_s - 7) && ((check_car_s-car_s) < 15)){
+			  way_is_clear = false;
+		  }
+
+		  //calculate the average speed of the vehicles in sight in the lane
+		  if((check_car_s > car_s) && ((check_car_s - car_s) < 50)){
+			  sum_speed += check_speed;
+			  num_of_cars += 1;
+		  }
+	  }
+	}
+	if(num_of_cars > 0){
+		lane_speed = sum_speed/num_of_cars;
+	}else{
+		lane_speed = 49.5;
+	}
+	return way_is_clear;
+}
 #endif  // HELPERS_H
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -50,8 +52,16 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+  //use the lane numbers from the Q&A video for simplicity
+  int lane = 1; //lane 1 is the middle lane
+  //use the reference velocity, too
+  double ref_vel = 0; //mph
+
+  string state = "KL"; //initialize in Keep Lane state
+
+
+  h.onMessage([&lane, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
+               &map_waypoints_dx,&map_waypoints_dy, &ref_vel, &state]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -78,33 +88,153 @@ int main() {
           double car_speed = j[1]["speed"];
 
           // Previous path data given to the Planner
-          auto previous_path_x = j[1]["previous_path_x"];
-          auto previous_path_y = j[1]["previous_path_y"];
+          vector<double> previous_path_x = j[1]["previous_path_x"];
+          vector<double> previous_path_y = j[1]["previous_path_y"];
           // Previous path's end s and d values 
           double end_path_s = j[1]["end_path_s"];
           double end_path_d = j[1]["end_path_d"];
 
           // Sensor Fusion Data, a list of all other cars on the same side 
           //   of the road.
-          auto sensor_fusion = j[1]["sensor_fusion"];
+          vector<vector<double>> sensor_fusion = j[1]["sensor_fusion"];
 
           json msgJson;
 
-          vector<double> next_x_vals;
-          vector<double> next_y_vals;
+          //size of the previous path
+          int prev_size = previous_path_x.size();
+          //if lane change was started in the previous message wait until most of the planned trajectory is traveled, then change the lane variable
+          if((state == "PLCL" || state == "PLCR") && (prev_size < 10)){
+        	  if(state == "PLCL"){
+        		  lane -= 1;
+        	  }else {
+        		  lane += 1;
+        	  }
+        	  state = "KL";
 
-          /**
-           * TODO: define a path made up of (x,y) points that the car will visit
-           *   sequentially every .02 seconds
-           */
+          }
+
+          /*
+		   * TODO: define a path made up of (x,y) points that the car will visit
+		   *   sequentially every .02 seconds
+		   */
+          //any evaluation is possible only after a lane change is completed
+          if(state != "PLCL" && state != "PLCR"){
+			  if(prev_size > 0){
+				  car_s = end_path_s;
+			  }
+			  bool too_close = false; //flag to show that a car is too close ahead
+			  bool change_left = false; //flag to shwm change left is safe
+			  bool change_right = false; //flag to show change right is safe
+			  vector<double> costs {10, 10, 10}; //costs for KL, LCL, LCR
+			  double follow_speed = 49.5; //speed to follow vehicle in lane if needed
 
 
-          msgJson["next_x"] = next_x_vals;
-          msgJson["next_y"] = next_y_vals;
+			  for(int i = 0; i < sensor_fusion.size(); i++){
+				float d = sensor_fusion[i][6]; //checking the i-th car's d position
+				//for cars in our lane
+				if(d < (2+4*lane+2) && d > (2+4*lane-2)){
+					double vx = sensor_fusion[i][3];
+					double vy = sensor_fusion[i][4];
+					double check_speed = sqrt(vx*vx+vy*vy)*2.24; //car speed along the road (magnitude of the speed vector) converted to mph
+					double check_car_s = sensor_fusion[i][5];
 
-          auto msg = "42[\"control\","+ msgJson.dump()+"]";
+					check_car_s += ((double)prev_size*.02*check_speed/2.24); //trying to predict where the car will be based on previous data
+					//and see if it is in front of us and is too close = closer than 30m
+					if((check_car_s > car_s) && ((check_car_s-car_s) < 30)){
+						//this car is too close
+						too_close = true;
+						double keep_cost = inefficiency_cost(49.5, check_speed, car_speed);
+						costs[0] = keep_cost - .02; //offset to slightly favor keeping lane
+						follow_speed = check_speed;
+					}
+				}
+			  }
 
-          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+				vector<string> possible_states;
+
+				//if too close decrease speed
+				if(too_close){
+					//check possible successor states
+					possible_states = successor_states(state, lane);
+					//check each possible successor state
+					for(int i=0; i < possible_states.size(); i++){
+						string s = possible_states[i];
+						if(s.compare("KL")==0){
+							state = "KL";
+
+						}else if(s.compare("PLCL") == 0){
+							int left_lane = lane - 1;
+							double left_lane_speed;
+							bool left_clear = is_lane_clear(sensor_fusion, left_lane, car_s, prev_size, left_lane_speed);
+							if(left_clear){
+
+								costs[1] = inefficiency_cost(49.5, left_lane_speed, car_speed);
+								change_left = true;
+
+							}
+						}else if(s.compare("PLCR") == 0){
+							int right_lane = lane + 1;
+							double right_lane_speed;
+							bool right_clear = is_lane_clear(sensor_fusion, right_lane, car_s, prev_size, right_lane_speed);
+							if(right_clear){
+
+								costs[2] = inefficiency_cost(49.5, right_lane_speed, car_speed);
+								change_right = true;
+							}
+						}
+					}
+				}
+				else if(ref_vel < 49){
+					ref_vel += .224; //normal acceleration
+				}
+				//get the minimum index item of the costs vector
+				vector<double>::iterator result = min_element(costs.begin(), costs.end());
+				int minCostIndex = std::distance(costs.begin(), result);
+				int new_lane = lane;
+
+				//perform the lane change
+				if(change_left && (minCostIndex == 1)){
+					if(ref_vel < 45){
+						ref_vel+= .2; //acceleration before lane change if speed is low
+					}
+					new_lane -= 1;
+					state = "PLCL";
+				}else if(change_right && (minCostIndex == 2)){
+					if(ref_vel < 45){
+						ref_vel+= .2; //acceleration before lane change if speed is low
+					}
+					new_lane += 1;
+					state = "PLCR";
+				}else if(ref_vel > follow_speed){
+						ref_vel -= 0.224; //deceleration to follow vehicle in lane
+				}
+
+
+			  //define the actual points to be set for the spline
+			  vector<double> next_x_vals;
+			  vector<double> next_y_vals;
+
+			  generate_trajectory(car_x,car_y,car_yaw,car_s,ref_vel,new_lane,prev_size,previous_path_x,previous_path_y,map_waypoints_s,map_waypoints_x,map_waypoints_y,next_x_vals,next_y_vals);
+
+
+			  msgJson["next_x"] = next_x_vals;
+			  msgJson["next_y"] = next_y_vals;
+
+
+			  auto msg = "42[\"control\","+ msgJson.dump()+"]";
+
+			  ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+          } //end of lane change checking if
+          else{
+        	  msgJson["next_x"] = previous_path_x; //while lane change is not complete the same trajectory is resent
+			  msgJson["next_y"] = previous_path_y; //(decreased with the already traveled parts)
+
+
+			  auto msg = "42[\"control\","+ msgJson.dump()+"]";
+
+			  ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+          }
+
         }  // end "telemetry" if
       } else {
         // Manual driving
